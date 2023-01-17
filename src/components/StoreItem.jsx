9afd7ebf941c16e68da2dab9b4c56eaba645cf75
Chyna <chynapblair@gmail.com>
2023-01-17 07:24:41 -0500

    added shopping cart functionality
    allow users to an add item to the cart while increasing the cart quantity

--- src/components/StoreItem.jsx
+++ src/components/StoreItem.jsx
@@ -1,8 +1,11 @@
 import { Button, Card } from "react-bootstrap"
+import { useShoppingCart } from "../context/ShoppingCartContext";
 import { formatCurrency } from "../utils/formatCurrency";
 
 export const StoreItem = ({ id, name, price, imgUrl }) => {
-    const quantity = 0;
+    const { getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart  } = useShoppingCart();
+    const quantity = getItemQuantity(id);
+
     return (
         <Card className="h-100">
             <Card.Img
@@ -19,21 +22,21 @@ export const StoreItem = ({ id, name, price, imgUrl }) => {
                 <div className="mt-auto">
 
                     {quantity === 0 ? (
-                        <Button className="w-100">+ Add To Cart</Button>
+                        <Button className="w-100" onClick={() => increaseCartQuantity(id)}>+ Add To Cart</Button>
                     ) : (
-                            <div className="d-flex align-items-center flex-column"
-                                style={{ gap: ".5rem"}}
-                                >
-                                <div className="d-flex align-items-center justify-content-center"
-                                style={{ gap: ".5rem"}}
-                                >
-                                <Button>-</Button>
-                                <div>
-                                <span className="fs-3">{quantity}</span> in cart
-                                </div>
-                                <Button>+</Button>
+                        <div className="d-flex align-items-center flex-column"
+                            style={{ gap: ".5rem"}}
+                            >
+                            <div className="d-flex align-items-center justify-content-center"
+                            style={{ gap: ".5rem"}}
+                            >
+                            <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
+                            <div>
+                            <span className="fs-3">{quantity}</span> in cart
                             </div>
-                        <Button variant="danger" size="sm">Remove</Button>
+                            <Button onClick={() => increaseCartQuantity(id)}>+</Button>
+                        </div>
+                        <Button variant="danger" size="sm" onClick={() => removeFromCart(id)}>Remove</Button>
                     </div>
                     )}
                 </div>
--- src/context/ShoppingCartContext.js
+++ src/context/ShoppingCartContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react"
+import { createContext, useContext, useState } from "react"
 
 const ShoppingCartContext = createContext({});
 
@@ -8,13 +8,60 @@ export const useShoppingCart = () => {
 
 export const ShoppingCartProvider = ({ children }) => {
     const [cartItems, setCartItems] = useState([]);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openCart = () => setIsOpen(true);
+    const closeCart = () => setIsOpen(false);
 
     const getItemQuantity = (id) => {
         return cartItems.find(item => item.id === id)?.quantity || 0
-    }
+    };
+
+    const increaseCartQuantity = (id) => {
+        setCartItems(currItems => {
+            if (currItems.find(item => item.id === id) == null) {
+                return [...currItems, { id, quantity: 1 }]
+            } else {
+                return currItems.map(item => {
+                    if (item.id === id) {
+                        return { ...item, quantity: item.quantity + 1 }
+                    } else {
+                        return item
+                    }
+                })
+            }
+        })
+    };
+
+    const decreaseCartQuantity = (id) => {
+        setCartItems(currItems => {
+            if (currItems.find(item => item.id === id)?.quantity === 1) {
+                return currItems.filter(item => item.id !== id)
+            } else {
+                return currItems.map(item => {
+                    if (item.id === id) {
+                        return { ...item, quantity: item.quantity - 1 }
+                    } else {
+                        return item
+                    }
+                })
+            }
+        })
+    };
+
+    const removeFromCart = (id) => {
+        setCartItems(currItems => {
+            return currItems.filter(item => item.id !== id)
+        })
+    };
+
+    const cartQuantity = cartItems.reduce(
+        (quantity, item) => item.quantity + quantity,
+        0
+    );
 
     return (
-        <ShoppingCartContext.Provider value={{ getItemQuantity }}>
+        <ShoppingCartContext.Provider value={{ getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart, cartItems, cartQuantity, openCart, closeCart }}>
             {children}
         </ShoppingCartContext.Provider>
     );
