1aa7c148c3aa5a05311ff0ca88eb78b7310c56ac
Chyna <chynapblair@gmail.com>
2023-01-31 06:28:30 -0500

    shopping cart context

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,1 +1,34 @@
-export const CartItem = ({ id, quantity }) => {};
+import { Stack } from 'react-bootstrap';
+import { useShoppingCart } from '../context/ShoppingCartContext';
+import storeItems from '../data/ITEMS.json';
+import { formatCurrency } from '../utils/formatCurrency';
+
+export const CartItem = ({ id, quantity }) => {
+  const { removeFromCart } = useShoppingCart();
+  const item = storeItems.find((i) => i.id === id);
+  if (item == null) {
+    return null;
+  }
+
+  return (
+    <Stack direction='horizontal' gap={2} className='d-flex align-items-center'>
+      <img
+        src={item.imgUrl}
+        style={{ width: '125px', height: '125px', objectFit: 'cover' }}
+      />
+      <div className='me-auto'>
+        <div>
+          {item.name}{' '}
+          {quantity > 1 && (
+            <span className='text-muted' style={{ fontSize: '.75rem' }}>
+              x{quantity}
+            </span>
+          )}
+        </div>
+        <div className='text-muted' style={{ fontSize: '.85rem' }}>
+          {formatCurrency(item.price)}
+        </div>
+      </div>
+    </Stack>
+  );
+};
